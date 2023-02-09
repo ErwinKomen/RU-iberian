e9a48cab30ca74f72113a7e681b9a717a90e25d0
ErwinKomen <E.Komen@ru.nl>
2023-02-09 16:29:18 +0100

    admin user overview

--- iberian/iberian/saints/admin.py
+++ iberian/iberian/saints/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import *
+from django.contrib.admin.models import LogEntry, DELETION
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from django.urls import reverse
+
+# Own application
+from iberian.saints.models import *
 
 # Register your models here.
 admin.site.register(Church)
@@ -9,3 +15,12 @@ admin.site.register(Bibliography)
 # @admin.register(Location)
 # class LocationAdmin(OSMGeoAdmin):
 #     list_display = ('coordinates',)
+
+
+# How to display user information
+admin.site.unregister(User)
+# What to display in a list
+UserAdmin.list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined', 'last_login']
+# Turn it on again
+admin.site.register(User, UserAdmin)
+
